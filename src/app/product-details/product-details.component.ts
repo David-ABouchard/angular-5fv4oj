ea053c8d356ca5ba22a29e7e348eea21d2fe128d
David-ABouchard <1426316@cegepoutaouais.qc.ca>
2022-09-01 13:25:25 -0400

    4 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -11,9 +11,12 @@ export class ProductDetailsComponent implements OnInit {
 
   product: Product | undefined;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+    this.product = products.find(product => product.id === productIdFromRoute);
   }
 
 }
